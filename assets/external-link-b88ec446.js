5148e11cef1491e26009ded26f5016e09b86270b
d3george <143698843+d3george@users.noreply.github.com>
2023-11-27 16:35:37 +0000

    Deploying to gh-pages from @ d3george/slash-admin@aff54badf3b13ee35f4f46c1fcfeddab66f1b79b 🚀

---
+++ assets/external-link-b88ec446.js
@@ -0,0 +1,1 @@
+import{b$ as e,a as o,A as s}from"./index-d8577bd1.js";function n({to:t}){const{back:a}=e();return o.useLayoutEffect(()=>{window.open(t,"_black"),a()}),s.jsx("div",{})}export{n as default};
